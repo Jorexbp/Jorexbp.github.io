63bec4aa8d4edfb65fe4e1e4a435864a6fc67fe9
Jorexbp <jorgebp2004@gmail.com>
2023-07-14 22:40:40 +0200

    Probemos suerte

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const observador = new IntersectionObserver((entr) => 
 entr.forEach((ent) => {
     if(ent.isIntersecting){
@@ -16,17 +15,3 @@ window.onload(window.scroll({
 	top: 0,
 	behavior: "smooth"
 }));
-=======
-const observador = new IntersectionObserver((entr) => 
-entr.forEach((ent) => {
-    if(ent.isIntersecting){
-ent.target.classList.add('show');
-    }else{
-        ent.target.classList.remove('show');
-    }
-})
-);
-
-const hidden = document.querySelectorAll('.hidden')
-hidden.forEach((ele) => observador.observe(ele));
->>>>>>> e62ad5d47e3ed0b75dcd59682eacc920eeb9bf7a
