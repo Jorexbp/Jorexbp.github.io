89bc56d3801f034821863b706b79ada3a06c41db
Jorexbp <jorgebp2004@gmail.com>
2023-07-26 20:11:46 +0200

    Responsivo de NavBar y sobremi, animaciones navBar

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const observador = new IntersectionObserver((entr) => 
 entr.forEach((ent) => {
     if(ent.isIntersecting){
-ent.target.classList.add('show');
+        if((ent.target.className.indexOf('hidNav') != -1)){
+            ent.target.classList.add('showNav');
+        }else{
+            ent.target.classList.add('show');
+        }
+
     }
+
 })
 );
 
-const hidden = document.querySelectorAll('.hidden')
+const hidden = document.querySelectorAll('.hidden');
+const hidNav = document.querySelectorAll('.hidNav');
+
+hidNav.forEach((ele) => observador.observe(ele));
 hidden.forEach((ele) => observador.observe(ele));
 
 window.onload(window.scroll({
