775ada7f8708760c33759d2cb38c9d2a3a33c252
Jorexbp <jorgebp2004@gmail.com>
2023-08-14 13:12:52 +0200

    Contacto gif en Github

--- contacto/contacto.js
+++ contacto/contacto.js
@@ -18,7 +18,21 @@ const hidNav = document.querySelectorAll('.hidNav');
 hidNav.forEach((ele) => observador.observe(ele));
 hidden.forEach((ele) => observador.observe(ele));
 
+const gitImage = document.getElementById('imagen');
+gitImage.addEventListener('mouseover',cambioAzul);
+gitImage.addEventListener('mouseleave',cambioRojo);
+function cambioRojo(){
+    const gif = document.getElementById('gif');
+    gif.style.visibility = 'hidden';
+}
+function cambioAzul(){
+    const gif = document.getElementById('gif');
+    gif.style.visibility = 'visible';
+}
+
 window.onload(window.scroll({
 	top: 0,
 	behavior: "smooth"
 }));
+
+
